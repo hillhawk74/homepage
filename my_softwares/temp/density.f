a9ab0b06a86a884fc1887bd51a326a280e9720c4
hillhawk74 <ju@math.sc.edu>
2026-08-14 15:25:30 -0400

    Delete my_softwares/temp directory

--- my_softwares/temp/density.f
+++
@@ -1,8 +0,0 @@
-C*******************************************************************
-C     Define the density function for the SCVT
-C*******************************************************************
-        function dens_f(pt)
-
-        dens_f = 1.0
-
-        end function dens_f
